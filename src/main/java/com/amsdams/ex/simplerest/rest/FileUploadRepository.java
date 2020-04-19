e5f3597b0577a0919f6efd55cf05ffe626bcae0e
ju la <>
2020-04-19 19:58:34 +0200

    this was deleted...

--- src/main/java/com/amsdams/ex/simplerest/rest/FileUploadRepository.java
+++
@@ -1,6 +0,0 @@
-package com.amsdams.ex.simplerest.rest;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FileUploadRepository extends JpaRepository<FileUpload, Long> {
-}
